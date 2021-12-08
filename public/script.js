d16b0124bfd66ad512a2c4db20d391cb80637184
Samuel0102 <damu.comsa@gmail.com>
2021-12-08 17:05:17 -0300

    deploy netlify

---
+++ public/script.js
@@ -0,0 +1,15 @@
+function showMenu(){
+    // getting dom elements
+    let hamburgerIcon = document.getElementById("hamburger-icon");
+    let hiddenMenu = document.getElementById("hidden-nav");
+
+    // verifing if the menu is or isnt showing at the click of the icon
+    if (hiddenMenu.style.display === "none"){
+        hiddenMenu.style.display = "block";
+        hamburgerIcon.setAttribute("src", "../../src/assets/icon-close.svg");
+    }else{
+        hiddenMenu.style.display = "none";
+        hamburgerIcon.setAttribute("src", "../../src/assets/icon-hamburger.svg");
+    }
+}
+
