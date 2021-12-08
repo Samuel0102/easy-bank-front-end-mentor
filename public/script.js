b6f7456c297e4f0086ce348a9710c04a1a3acda9
Samuel0102 <damu.comsa@gmail.com>
2021-12-08 17:08:54 -0300

    arrumando erro decaminho

--- public/script.js
+++ public/script.js
@@ -6,10 +6,10 @@ function showMenu(){
     // verifing if the menu is or isnt showing at the click of the icon
     if (hiddenMenu.style.display === "none"){
         hiddenMenu.style.display = "block";
-        hamburgerIcon.setAttribute("src", "../../src/assets/icon-close.svg");
+        hamburgerIcon.setAttribute("src", "assets/icon-close.svg");
     }else{
         hiddenMenu.style.display = "none";
-        hamburgerIcon.setAttribute("src", "../../src/assets/icon-hamburger.svg");
+        hamburgerIcon.setAttribute("src", "assets/icon-hamburger.svg");
     }
 }
 
